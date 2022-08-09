9c80897a534eb8dbe5048a81b9fdf373b677cc31
Stone <65476500+StoneBelle@users.noreply.github.com>
2022-08-09 18:05:09 -0400

    Add files via upload

--- homescreen.py
+++ homescreen.py
@@ -1,16 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter.messagebox import showerror, showinfo
+from tkinter import messagebox, END
+import json
 
 # GUI CONSTANTS
 BG_COL = "#FFFFFF"
-OPTNL_BTN = "#EECF7F"
-MNDTRY_BTN = "#EF967A"
 FONT = "Arial", 8, "bold"
 
 
 # TODO #1: Create Home Screen
-
 # Class that inherits tk.Tk to create a window object
 class AppWindow(tk.Tk):
     def __init__(self, title):
@@ -21,23 +19,25 @@ class AppWindow(tk.Tk):
         # img = tk.PhotoImage(file="logo.png")
         # self_logo =tk.Label(self, image=img)
         # self_logo.grid(column=0, row=1)
+
         # Window Responsiveness
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
 
 
-# Class that inherits ttk.Frame to create a Frame object in a window
+# Class that inherits ttk.Frame to create a Frame object inside a window
+# The window created from the AppWindow class will be passed in as the 'root' for this Frame
 class AppFrame(ttk.Frame):
-    def __init__(self, root):  # The window created from AppWindow class will be passed as the 'root' for this Frame
+    def __init__(self, root):
         super().__init__(root)
 
         # ttk Widget Styles
         style = ttk.Style()
         style.configure("TFrame", background=BG_COL)
         style.configure("TButton", background=BG_COL)
-        style.configure("TLabel", background=BG_COL, font=("Arial", 8, "bold"))
+        style.configure("TLabel", background=BG_COL, font=FONT)
 
-        # Widgets
+        # ttk Widgets
         self.prompt_label = ttk.Label(self, text="Save or find your Account login below.")
         self.acc_label = ttk.Label(self, text="Account :  ")
         self.user_label = ttk.Label(self, text="Username :  ")
@@ -45,13 +45,13 @@ class AppFrame(ttk.Frame):
 
         self.acc_entry = ttk.Entry(self, width=36)
         self.user_entry = ttk.Entry(self, width=55)
-        self.pass_entry = ttk.Entry(self, width=36)
+        self.pass_entry = ttk.Entry(self, width=36, show="*")
 
         self.find_button = ttk.Button(self, text="Find Login", width=17)
         self.pass_button = ttk.Button(self, text="Generate Password", width=17)
-        self.add_button = ttk.Button(self, text="Save Login", width=53)
+        self.add_button = ttk.Button(self, text="Save Login", width=53, command=self.save_login)
 
-        # Widget grid
+        # ttk Widget grid
         self.prompt_label.grid(column=3, columnspan=2, row=1, rowspan=2, pady=(0, 20))
         self.acc_label.grid(column=2, row=2, padx=(8, 0), pady=(50, 0))
         self.user_label.grid(column=2, row=3)
@@ -71,6 +71,28 @@ class AppFrame(ttk.Frame):
         self.grid()
 
     def save_login(self):
-        pass
+        account = self.acc_entry.get()
+        username = self.user_entry.get()
+        password = self.pass_entry.get()
 
-# Attach Menu Bar to Home Screen
+        login_data = {account: {
+            "username": username,
+            "password": password}
+        }
+
+        if len(account) == 0 and len(username) == 0 and len(password) == 0:
+            messagebox.showerror(title="Missing Required Fields", message="You must fill in all fields to continue.")
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)  # Reading old data
+        except FileNotFoundError:  # This will occur if file is not found (i.e. if try does not work)
+            with open("data.json", "w") as data_file:
+                json.dump(login_data, data_file, indent=4)  # Write new data
+        else:  # This will occur if file is found (i.e. if try does work)
+            with open("data.json", "w") as data_file:
+                data.update(login_data)  # Updating old data with new data
+                json.dump(data, data_file, indent=4)  # Saving updated data
+
+        self.acc_entry.delete(0, END)
+        self.user_entry.delete(0, END)
+        self.pass_entry.delete(0, END)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from homescreen import AppWindow, AppFrame
+from tkinter import *
+
 # Account Simple 4.0
 
 
@@ -7,15 +9,33 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
            'w', 'x', 'y', 'z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+menu_items = ("Update Login", "Delete Login", "Portrait View", "Landscape View", "View All", "Tutorial", "FAQ")
 
-app = AppWindow("Hello World")
+
+def make_menu(menu_tab, start, end, tf):
+    tab = Menu(menu_bar, tearoff=False)
+    count = 0
+    for item in menu_items[slice(start, end)]:
+        count += 1
+        tab.add_command(label=item)
+        if tf and count == 2:
+            tab.add_separator()
+    menu_bar.add_cascade(label=menu_tab, menu=tab)
+
+
+# Root Window
+app = AppWindow("Account Simple  4.0")
+
+# Menu bar for Root Window
+menu_bar = Menu(app)
+app.config(menu=menu_bar)
+make_menu("Edit", 0, 2, False)
+make_menu("View", 2, 5, True)
+make_menu("Help", 5, 8, False)
+
 frame = AppFrame(app)
+
 app.mainloop()
-
-
-
-
-
 
 # TODO #2 : Create Menu Bar
 
@@ -28,16 +48,3 @@ app.mainloop()
 # Create a Scrollbar
 
 # TODO #3: Create JSON  functions to read, write, and update data
-
-
-# TODO : Edit Login
-
-#
-
-# TODO : Delete Login
-
-
-# TODO : View All
-
-
-#
