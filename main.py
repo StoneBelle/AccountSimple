5067856cbaa1c9600dcd1973a457ac4defb74094
Stone <65476500+StoneBelle@users.noreply.github.com>
2022-09-26 20:57:01 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,29 +2,38 @@ from homescreen import AppWindow, AppFrame
 from menu import *
 from tkinter import *
 from modify_data import *
+import time
 
-# # Root Window
-# app = AppWindow("Account Simple  4.0")
-# frame = AppFrame(app)
-# menu = MenuBar(app)
-# app.mainloop()
-
-# Create Menu bar for the
-# homescreen
 MENU_ITEMS = ("Update Login", "Delete Login", "Portrait View", "Landscape View", "View All", "Tutorial", "FAQ")
+DISABLE_STATE = ("Update Login", "Delete Login", "View All")
 
 
 def make_menu(menu_tab, start, end, tf, cmd):
-    """Create a custom Menu bar by filling the parameters.
-    """
+    """Create a custom Menu bar by filling the parameters."""
     tab = Menu(menu_bar, tearoff=False)
     count = 0
-    for item in MENU_ITEMS[slice(start, end)]:
-        count += 1
+
+    try:  # Checking for saved data
+        read_data()
+    except FileNotFoundError:
+        for item in MENU_ITEMS[slice(start, end)]:
+            count += 1
+            if tf and count == 2:
+                tab.add_separator()
+            if item in DISABLE_STATE:
+                tab.add_command(label=item, command=cmd, state=DISABLED)
+            else:
+                tab.add_command(label=item, command=cmd)
+    else:  # If existing data found, retrieve login info using input from account
+        for item in MENU_ITEMS[slice(start, end)]:
+            count += 1
+            if tf and count == 2:
+                tab.add_separator()
         tab.add_command(label=item, command=cmd)
-        if tf and count == 2:
-            tab.add_separator()
-    menu_bar.add_cascade(label=menu_tab, menu=tab)
+    finally:
+        menu_bar.add_cascade(label=menu_tab, menu=tab)
+
+
 
 # Root Window
 app = AppWindow("Account Simple  4.0")
